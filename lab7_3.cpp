627b26330e24ab3087899cd2713639960ed3c29c
Natee <gg977120@gmail.com>
2020-12-29 15:12:30 +0700

    write code by Flowchart - Done.

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -9,3 +9,74 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    double age;
+    double height;
+    double asset;
+    string status;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<=20)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height<160)
+        {
+            status = "UNFRIEND";
+        }
+        else
+        {
+            if(height<175)
+            {
+                status = "FRIEND";
+            }
+            else
+            {
+                cout << "Enter your property: ";
+                cin >> asset;
+                if(asset>69e6)
+                {
+                    status = "MARRIED";
+                }
+                else
+                {
+                    status = "ONE-NIGHT-STAND";
+                }
+                
+            }
+            
+        }
+        
+    }
+    else
+    {
+        if(age<30)
+        {
+            cout << "Enter your property: ";
+            cin >> asset;
+            if(asset>10e6)
+            {
+                status = "BEST FRIEND";
+            }
+            else
+            {
+                status = "UNFRIEND";
+            }
+            
+        }
+        else
+        {
+            status = "UNFRIEND";
+        }
+        
+    }
+
+    cout << "Your status = " << status;
+
+    return 0;
+}
